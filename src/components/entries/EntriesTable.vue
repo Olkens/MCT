d712652753871f8ea51e64c32b1872e96c10a3c3
“Olkens” <olkusz.michal@gmail.com>
2025-02-09 16:55:29 +0100

    impletmented unocss and addes some styles

--- src/components/entries/EntriesTable.vue
+++ src/components/entries/EntriesTable.vue
@@ -1,23 +1,44 @@
 <template>
-<ul>
-  <ListItem v-for="entry in entries" :key="entry.id" entry-name="entry.test">
-    {{entry.test}}
-  </ListItem>
-</ul>
-  <button @click="add">hehe</button>
+  <div class="flex flex-col items-center justify-center h-full">
+    <ul v-for="entry in entries" :key="entry.id">
+      <ListItem :entry="entry"/>
+    </ul>
+    <button @click="add">hehe</button>
+  </div>
 </template>
 <script setup>
 import supabase from "../../services/supabase.js";
 import {onMounted, ref} from "vue";
 import ListItem from "./ListItem.vue";
 
-let {data: entries, error} = ref(await supabase
-    .from('test')
-    .select('test')
-)
+const entries = ref([])
+
+async function fetchEntries() {
+  try {
+    let {data: data, error} = await supabase
+        .from('test')
+        .select('*')
+    entries.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  fetchEntries()
+})
+
 
 const add = () => {
-  entries.push({id: 5, date: 1, test: 'uwu'})
-  console.log(entries)
+  entries.value.push({id: 5, date: 1, test: 'uwu'})
 }
 </script>
+
+<style scoped>
+ul {
+  color: #fff;
+  border: 1px solid #fff;
+  border-radius: 15px;
+  padding: 8px 12px;
+}
+</style>
--- src/components/entries/ListItem.vue
+++ src/components/entries/ListItem.vue
@@ -1,8 +1,30 @@
 <template>
-<li>{{entryName}}</li>
-  hehe
+  <div>
+    <span>{{ entry.id }} </span>
+    <span>{{ entry.name }} </span>
+    <span>{{ entry.amount }} </span>
+    <span>{{entry.payments_left}}</span>
+    <span>add</span>
+    <span>delete</span>
+  </div>
 </template>
 
 <script setup>
-defineProps(['entryName'])
+defineProps(['entry'])
 </script>
+
+<style scoped>
+div {
+  color: #fff;
+}
+
+span {
+  padding: 8px 12px;
+
+}
+
+li {
+  text-decoration: none;
+  list-style: none;
+}
+</style>
